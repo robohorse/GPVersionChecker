44a15ed53b555a1eda54c70dc3420871cf5ebba6
robohorse <robohorsed@gmail.com>
2016-08-05 12:57:57 +0300

    parser tests updated

--- src/test/java/com/robohorse/gpversionchecker/DataParserTest.java
+++ src/test/java/com/robohorse/gpversionchecker/DataParserTest.java
@@ -13,9 +13,10 @@ import org.mockito.MockitoAnnotations;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 
 /**
@@ -64,6 +65,117 @@ public class DataParserTest {
 
         assertNotNull("VersionVO is empty", version);
         assertFalse("App update is required, but should be not", version.isNeedToUpdate());
+    }
+
+    @Test
+    public void testDataParsing_isCorrectChangesReading() throws Exception {
+        final String currentVersion = "1.9.48";
+        String htmlBody = assetsReader.read("sample_app.html");
+        Document document = Jsoup.parse(htmlBody);
+        final Version version = dataParser.parse(document, currentVersion, URL);
+
+        assertNotNull("VersionVO is empty", version);
+        assertTrue("App changes was not parsed correct",
+                "design".equals(version.getChanges()));
+    }
+
+    @Test
+    public void testDataParsing_isCorrectDescriptionReading() throws Exception {
+        final String currentVersion = "1.9.48";
+        String htmlBody = assetsReader.read("sample_app.html");
+        Document document = Jsoup.parse(htmlBody);
+        final Version version = dataParser.parse(document, currentVersion, URL);
+
+        assertNotNull("VersionVO is empty", version);
+        assertTrue("App description was not parsed correct",
+                "Awesome app".equals(version.getDescription()));
+    }
+
+    @Test
+    public void testDataParsing_isCorrectOnEmptyUserAppVersion() throws Exception {
+        final String currentVersion = null;
+        String htmlBody = assetsReader.read("sample_app.html");
+        Document document = Jsoup.parse(htmlBody);
+        final Version version = dataParser.parse(document, currentVersion, URL);
+
+        assertNull("VersionVO is not empty", version);
+    }
+
+    @Test
+    public void testDataParsing_isCorrectOnEmptyCurrentAppVersion() throws Exception {
+        final String currentVersion = "1.9.48";
+        String htmlBody = assetsReader.read("sample_app_empty_version.html");
+        Document document = Jsoup.parse(htmlBody);
+        final Version version = dataParser.parse(document, currentVersion, URL);
+
+        assertNull("VersionVO is not empty", version);
+    }
+
+    @Test
+    public void testDataParsing_isCorrectOnLowerUserVersionWithCharacters() throws Exception {
+        final String currentVersion = "1";
+        String htmlBody = assetsReader.read("sample_app_symbol_version.html");
+        Document document = Jsoup.parse(htmlBody);
+        final Version version = dataParser.parse(document, currentVersion, URL);
+
+        assertNotNull("VersionVO is empty", version);
+        assertTrue("App update is not required, but should be", version.isNeedToUpdate());
+    }
+
+    @Test
+    public void testDataParsing_isCorrectOnHigherUserVersionWithCharacters() throws Exception {
+        final String currentVersion = "1.9.49";
+        String htmlBody = assetsReader.read("sample_app_symbol_version.html");
+        Document document = Jsoup.parse(htmlBody);
+        final Version version = dataParser.parse(document, currentVersion, URL);
+
+        assertNotNull("VersionVO is empty", version);
+        assertFalse("App update is required, but should be not", version.isNeedToUpdate());
+    }
+
+    @Test
+    public void testDataParsing_isCorrectOnSameUserVersionWithCharacters() throws Exception {
+        final String currentVersion = "1.9.48";
+        String htmlBody = assetsReader.read("sample_app_symbol_version.html");
+        Document document = Jsoup.parse(htmlBody);
+        final Version version = dataParser.parse(document, currentVersion, URL);
+
+        assertNotNull("VersionVO is empty", version);
+        assertFalse("App update is required, but should be not", version.isNeedToUpdate());
+    }
+
+    @Test
+    public void testDataParsing_isCorrectChangesReadingWithCharacters() throws Exception {
+        final String currentVersion = "1.9.48";
+        String htmlBody = assetsReader.read("sample_app_symbol_version.html");
+        Document document = Jsoup.parse(htmlBody);
+        final Version version = dataParser.parse(document, currentVersion, URL);
+
+        assertNotNull("VersionVO is empty", version);
+        assertTrue("App changes was not parsed correct",
+                "design".equals(version.getChanges()));
+    }
+
+    @Test
+    public void testDataParsing_isCorrectDescriptionReadingWithCharacters() throws Exception {
+        final String currentVersion = "1.9.48";
+        String htmlBody = assetsReader.read("sample_app_symbol_version.html");
+        Document document = Jsoup.parse(htmlBody);
+        final Version version = dataParser.parse(document, currentVersion, URL);
+
+        assertNotNull("VersionVO is empty", version);
+        assertTrue("App description was not parsed correct",
+                "Awesome app".equals(version.getDescription()));
+    }
+
+    @Test
+    public void testDataParsing_isCorrectOnEmptyUserAppVersionWithCharacters() throws Exception {
+        final String currentVersion = null;
+        String htmlBody = assetsReader.read("sample_app_symbol_version.html");
+        Document document = Jsoup.parse(htmlBody);
+        final Version version = dataParser.parse(document, currentVersion, URL);
+
+        assertNull("VersionVO is not empty", version);
     }
 }
 
