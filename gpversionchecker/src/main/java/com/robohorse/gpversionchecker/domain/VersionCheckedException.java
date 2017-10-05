8cbd995dd1330d5d7c758ef7129d61154794dd4c
Vadim Schenev <robohorsed@gmail.com>
2017-10-05 16:02:27 +0300

    ServiceStartManagerTest tests added

--- gpversionchecker/src/main/java/com/robohorse/gpversionchecker/domain/VersionCheckedException.java
+++ gpversionchecker/src/main/java/com/robohorse/gpversionchecker/domain/VersionCheckedException.java
@@ -4,7 +4,7 @@ package com.robohorse.gpversionchecker.domain;
  * Created by v.shchenev on 05/10/2017.
  */
 
-public class VersionCheckedException extends IllegalStateException {
+public class VersionCheckedException extends Exception {
     public VersionCheckedException() {
         super("Version already checked");
     }
--- gpversionchecker/src/test/java/com/robohorse/gpversionchecker/ServiceStartManagerTest.java
+++ gpversionchecker/src/test/java/com/robohorse/gpversionchecker/ServiceStartManagerTest.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 
 import com.robohorse.gpversionchecker.domain.CheckingStrategy;
+import com.robohorse.gpversionchecker.domain.Version;
+import com.robohorse.gpversionchecker.domain.VersionCheckedException;
 import com.robohorse.gpversionchecker.manager.ServiceStartManager;
 import com.robohorse.gpversionchecker.provider.SharedDataProvider;
 import com.robohorse.gpversionchecker.utils.DateFormatUtils;
@@ -20,6 +22,8 @@ import org.robolectric.annotation.Config;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -70,5 +74,101 @@ public class ServiceStartManagerTest {
 
         manager.checkAndStartService(activity, CheckingStrategy.ONE_PER_VERSION_PER_DAY);
         verify(activity, atLeastOnce()).startService(new Intent(activity, VersionCheckerService.class));
+    }
+
+    @Test
+    public void testOnResult_one_per_version() throws Exception {
+        when(sharedDataProvider.provideLastVersionCode()).thenReturn("1");
+        final Version version = new Version
+                .Builder()
+                .setNewVersionCode("1")
+                .build();
+        VersionCheckedException exception = null;
+        try {
+            manager.onResulted(CheckingStrategy.ONE_PER_VERSION, version);
+        } catch (VersionCheckedException e) {
+            exception = e;
+        }
+        assertNotNull(exception);
+    }
+
+    @Test
+    public void testOnResult_one_per_version_per_day() throws Exception {
+        when(sharedDataProvider.provideLastVersionCode()).thenReturn("1");
+        final Version version = new Version
+                .Builder()
+                .setNewVersionCode("1")
+                .build();
+        VersionCheckedException exception = null;
+        try {
+            manager.onResulted(CheckingStrategy.ONE_PER_VERSION_PER_DAY, version);
+        } catch (VersionCheckedException e) {
+            exception = e;
+        }
+        assertNotNull(exception);
+    }
+
+    @Test
+    public void testOnResult_one_per_version_isValid() throws Exception {
+        when(sharedDataProvider.provideLastVersionCode()).thenReturn("1");
+        final Version version = new Version
+                .Builder()
+                .setNewVersionCode("2")
+                .build();
+        VersionCheckedException exception = null;
+        try {
+            manager.onResulted(CheckingStrategy.ONE_PER_VERSION, version);
+        } catch (VersionCheckedException e) {
+            exception = e;
+        }
+        assertNull(exception);
+    }
+
+    @Test
+    public void testOnResult_one_per_version_per_day_isValid() throws Exception {
+        when(sharedDataProvider.provideLastVersionCode()).thenReturn("1");
+        final Version version = new Version
+                .Builder()
+                .setNewVersionCode("2")
+                .build();
+        VersionCheckedException exception = null;
+        try {
+            manager.onResulted(CheckingStrategy.ONE_PER_VERSION_PER_DAY, version);
+        } catch (VersionCheckedException e) {
+            exception = e;
+        }
+        assertNull(exception);
+    }
+
+    @Test
+    public void testOnResult_one_per_day_isValid() throws Exception {
+        when(sharedDataProvider.provideLastVersionCode()).thenReturn("1");
+        final Version version = new Version
+                .Builder()
+                .setNewVersionCode("1")
+                .build();
+        VersionCheckedException exception = null;
+        try {
+            manager.onResulted(CheckingStrategy.ONE_PER_DAY, version);
+        } catch (VersionCheckedException e) {
+            exception = e;
+        }
+        assertNull(exception);
+    }
+
+    @Test
+    public void testOnResult_always_isValid() throws Exception {
+        when(sharedDataProvider.provideLastVersionCode()).thenReturn("1");
+        final Version version = new Version
+                .Builder()
+                .setNewVersionCode("1")
+                .build();
+        VersionCheckedException exception = null;
+        try {
+            manager.onResulted(CheckingStrategy.ALWAYS, version);
+        } catch (VersionCheckedException e) {
+            exception = e;
+        }
+        assertNull(exception);
     }
 }
